c4a83c973482a376daf4b4dd5e36e443eff0c860
Paul Craig <paul_craig_16@hotmail.com>
2019-03-23 15:50:47 -0400

    Clean up the test
    Test for a new message if a name is set.

--- src/test/java/action/HelloTest.java
+++ src/test/java/action/HelloTest.java
@@ -1,25 +1,33 @@
 package action;
 
 
-import com.opensymphony.xwork2.ActionProxy;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.StrutsTestCase;
 
-import org.junit.Test;
 
 public class HelloTest extends StrutsTestCase {
 
-    @Test
-    public void testExecuteValidationPasses() throws Exception {
+    public void testGetDefaultMessage() throws Exception {
 
-        ActionProxy actionProxy = getActionProxy("/hello.action") ;
+        HelloAction ha = container.inject(HelloAction.class);
 
-        HelloAction action = (HelloAction) actionProxy.getAction();
-
-        assertNotNull("The action is null but should not be.", action);
-
-        String result = actionProxy.execute();
+        String result = ha.execute();
 
         assertEquals("The execute method did not return " + ActionSupport.SUCCESS + " but should have.", ActionSupport.SUCCESS, result);
+
+        assertEquals("Expected the default message!", ha.getMessageStore().getMessage(), "Bonjour! Comment ça va?");
+    }
+
+    public void testSetNameInMessage() throws Exception {
+
+        HelloAction ha = container.inject(HelloAction.class);
+
+        ha.setName("Paul");
+
+        String result = ha.execute();
+
+        assertEquals("The execute method did not return " + ActionSupport.SUCCESS + " but should have.", ActionSupport.SUCCESS, result);
+
+        assertEquals("A message including the name ‘Paul’!", ha.getMessageStore().getMessage(), "Bonjour Paul! Comment ça va?");
     }
 }
